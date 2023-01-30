408904b43ec0556472d708d5aefe957171f32e39
Mateodiaz401 <mateodiaz401@gmail.com>
2023-01-30 15:44:40 -0500

    delete dist folder
    delete dist folder

--- dist/interfaces/car.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-//un interface es como un contrasto no tiene lógica de programación, es  solo una declaración propiedad y tipo de dato
-Object.defineProperty(exports, "__esModule", { value: true });
